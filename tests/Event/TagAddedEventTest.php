91aacb1dfa37f4651100d90c81c69b3c1127e8cc
Joachim Løvgaard <joachim@loevgaard.dk>
2022-07-12 20:16:53 +0200

    Simplify test

--- tests/Event/TagAddedEventTest.php
+++ tests/Event/TagAddedEventTest.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace Setono\TagBag\Event;
 
 use PHPUnit\Framework\TestCase;
+use Setono\TagBag\Renderer\ContentRenderer;
 use Setono\TagBag\Tag\ContentAwareTag;
 use Setono\TagBag\Tag\Rendered\RenderedTag;
-use Setono\TagBag\Tag\TagInterface;
-use Setono\TagBag\TagBagInterface;
+use Setono\TagBag\TagBag;
 
 /**
  * @covers \Setono\TagBag\Event\TagAddedEvent
@@ -22,61 +22,7 @@ final class TagAddedEventTest extends TestCase
     {
         $tag = ContentAwareTag::create('value');
         $renderedTag = RenderedTag::createFromTag($tag, 'value', 'fingerprint');
-
-        $tagBag = new class() implements TagBagInterface {
-            public function count(): int
-            {
-                throw new \RuntimeException('Not implemented');
-            }
-
-            public function add(TagInterface $tag): void
-            {
-                throw new \RuntimeException('Not implemented');
-            }
-
-            public function getAll(): array
-            {
-                throw new \RuntimeException('Not implemented');
-            }
-
-            public function getSection(string $section): array
-            {
-                throw new \RuntimeException('Not implemented');
-            }
-
-            public function hasSection(string $section): bool
-            {
-                throw new \RuntimeException('Not implemented');
-            }
-
-            public function renderAll(): string
-            {
-                throw new \RuntimeException('Not implemented');
-            }
-
-            public function renderSection(string $section): string
-            {
-                throw new \RuntimeException('Not implemented');
-            }
-
-            public function store(): void
-            {
-            }
-
-            public function restore(): void
-            {
-            }
-
-            public function get(string $name): RenderedTag
-            {
-                throw new \RuntimeException('Not implemented');
-            }
-
-            public function has(string $name): bool
-            {
-                throw new \RuntimeException('Not implemented');
-            }
-        };
+        $tagBag = new TagBag(new ContentRenderer());
 
         $event = new TagAddedEvent($renderedTag, $tagBag);
         self::assertSame($renderedTag, $event->tag);
