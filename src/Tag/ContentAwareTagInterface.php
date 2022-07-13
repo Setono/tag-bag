6f31c0f5d9dee9e5e06877986676d591ce74db13
Joachim Løvgaard <joachim@loevgaard.dk>
2022-07-13 08:21:20 +0200

    Do not extend tag interface

--- src/Tag/ContentAwareTagInterface.php
+++ src/Tag/ContentAwareTagInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Setono\TagBag\Tag;
 
-interface ContentAwareTagInterface extends TagInterface
+interface ContentAwareTagInterface
 {
     public function getContent(): string;
 }
